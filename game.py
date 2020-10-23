20bd6a85e268d1c690e982910f91c5ed8ee64391
AAAA <dev@onerbs.com>
2020-10-22 22:13:14 -0500

    Change code style

--- game.py
+++ game.py
@@ -2,41 +2,44 @@ from operator import add, sub
 
 
 class Side:
-	def __init__(self, o: int, x: int):
-		self.o = o
-		self.x = x
+    def __init__(self, o: int, x: int):
+        self.o = o
+        self.x = x
 
-	def is_valid(self):
-		if 0 < self.o < self.x:
-			return False
-		return self.o >= 0 and self.x >= 0
+    def is_valid(self):
+        if 0 < self.o < self.x:
+            return False
+        return self.o >= 0 and self.x >= 0
 
-	def __str__(self):
-		return 'O' * self.o + 'X' * self.x
+    def __str__(self):
+        return 'O' * self.o + 'X' * self.x
 
 
 class State:
-	T = tuple[int, int]
-	MOVES = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
+    T = tuple[int, int]
+    MOVES = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
 
-	def __init__(self, left: T, right: T, _phase=True, pid=''):
-		self.ls = Side(*left)
-		self.rs = Side(*right)
-		self._phase = _phase
-		self.pid = pid
+    def __init__(self, left: T, right: T, _phase=True, pid=''):
+        self.ls = Side(*left)
+        self.rs = Side(*right)
+        self._phase = _phase
+        self.pid = pid
 
-	def step(self):
-		lop = sub if self._phase else add
-		rop = add if self._phase else sub
-		moves = [State(
-			(lop(self.ls.o, move[0]), lop(self.ls.x, move[1])),
-			(rop(self.rs.o, move[0]), rop(self.rs.x, move[1])),
-			not self._phase, self.sid()
-		) for move in State.MOVES]
-		return [mv for mv in moves if mv.sid() != self.pid and mv.is_valid()]
+    def step(self):
+        lop = [add, sub][self._phase]
+        rop = [sub, add][self._phase]
+        moves = [State(
+            (lop(self.ls.o, move[0]), lop(self.ls.x, move[1])),
+            (rop(self.rs.o, move[0]), rop(self.rs.x, move[1])),
+            not self._phase, self.sid()
+        ) for move in State.MOVES]
+        return [mv for mv in moves if (
+            mv.sid() != '[OOOXXX:]'  # todo: save ref? ╮(￣_￣')╭
+            and mv.sid() != self.pid
+            and mv.is_valid())]
 
-	def is_valid(self):
-		return self.ls.is_valid() and self.rs.is_valid()
+    def is_valid(self):
+        return self.ls.is_valid() and self.rs.is_valid()
 
-	def sid(self):
-		return f'[{self.ls}:{self.rs}]'
+    def sid(self):
+        return f'[{self.ls}:{self.rs}]'
--- solution.py
+++ solution.py
@@ -2,17 +2,17 @@ from game import State
 
 
 def solution() -> list[str]:
-	curr = State((3, 3), (0, 0))
-	goal = State((0, 0), (3, 3))
-	path = []
-	while True:
-		path.append(curr.sid())
-		for branch in curr.step():
-			if branch.sid() == goal.sid():
-				path.append(branch.sid())
-				return path
-			curr = branch
+    curr = State((3, 3), (0, 0))
+    goal = State((0, 0), (3, 3))
+    path = []
+    while True:
+        path.append(curr.sid())
+        for branch in curr.step():
+            if branch.sid() == goal.sid():
+                path.append(branch.sid())
+                return path
+            curr = branch
 
 
 if __name__ == '__main__':
-	[print(step) for step in solution()]
+    [print(step) for step in solution()]
